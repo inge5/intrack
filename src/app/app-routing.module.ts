c3627c59828a43b4800146c14a13a29df98e987c
Nelson Zambrano <inge5neuromedia@gmail.com>
2021-12-22 16:17:40 -0500

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntrackComponent } from './intrack/intrack.component';
+import { SomosComponent } from './somos/somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IntrackComponent},
+  { path: 'somos', component: SomosComponent},
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    // scrollOffset: [0, 64],
+    useHash: true, initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimateContactoComponent } from './animate-contacto/animate-contacto.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { IntrackComponent, SafeHtmlPipe } from './intrack/intrack.component';
+import { SidebarContactComponent } from './sidebar-contact/sidebar-contact.component';
+import { SidebarComponent } from './intrack/sidebar/sidebar.component';
+import { SubmenuComponent } from './intrack/submenu/submenu.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SomosComponent } from './somos/somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimateContactoComponent,
+    FooterComponent,
+    HeaderComponent,
+    IntrackComponent,
+    SidebarContactComponent,
+    SidebarComponent,
+    SubmenuComponent,
+    SafeHtmlPipe,
+    SomosComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    CarouselModule,
+    NgxSkeletonLoaderModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
