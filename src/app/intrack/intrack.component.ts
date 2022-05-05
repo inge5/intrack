c51dc7c140074d29aece9e94026f6fce5dff99e1
Nelson Zambrano <inge5neuromedia@gmail.com>
2022-05-04 23:57:16 -0500

    ultimos cambios

--- src/app/intrack/intrack.component.ts
+++ src/app/intrack/intrack.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit, Pipe, PipeTransform, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Pipe,
+  PipeTransform,
+  ViewChild,
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl } from '@angular/platform-browser';
+import {
+  DomSanitizer,
+  SafeHtml,
+  SafeResourceUrl,
+  SafeScript,
+  SafeStyle,
+  SafeUrl,
+} from '@angular/platform-browser';
 import { HomeService } from '../services/home.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import * as AOS from 'aos';
 import { CommonService } from '../services/common.service';
 
-declare var $ : any; 
+declare var $: any;
 
-@Pipe({ name: 'safeHtml'})
-export class SafeHtmlPipe implements PipeTransform  {
+@Pipe({ name: 'safeHtml' })
+export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitized: DomSanitizer) {}
-  public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+  public transform(
+    value: any,
+    type: string
+  ): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
     // return this.sanitized.bypassSecurityTrustHtml(value);
     switch (type) {
-      case 'html': return this.sanitized.bypassSecurityTrustHtml(value);
-      case 'style': return this.sanitized.bypassSecurityTrustStyle(value);
-      case 'script': return this.sanitized.bypassSecurityTrustScript(value);
-      case 'url': return this.sanitized.bypassSecurityTrustUrl(value);
-      case 'resourceUrl': return this.sanitized.bypassSecurityTrustResourceUrl(value);
-      default: throw new Error(`Invalid safe type specified: ${type}`);
+      case 'html':
+        return this.sanitized.bypassSecurityTrustHtml(value);
+      case 'style':
+        return this.sanitized.bypassSecurityTrustStyle(value);
+      case 'script':
+        return this.sanitized.bypassSecurityTrustScript(value);
+      case 'url':
+        return this.sanitized.bypassSecurityTrustUrl(value);
+      case 'resourceUrl':
+        return this.sanitized.bypassSecurityTrustResourceUrl(value);
+      default:
+        throw new Error(`Invalid safe type specified: ${type}`);
     }
   }
 }
@@ -27,26 +49,31 @@ export class SafeHtmlPipe implements PipeTransform  {
 @Component({
   selector: 'app-intrack',
   templateUrl: './intrack.component.html',
-  styleUrls: ['./intrack.component.css']
+  styleUrls: ['./intrack.component.css'],
 })
 export class IntrackComponent implements OnInit {
   public userside: any;
-  data:any = [];
+  data: any = [];
   loader = true;
-  
-  constructor(private _sanitizer: DomSanitizer, private _homeservice:HomeService, private common: CommonService) { 
 
-  }
+  constructor(
+    private _sanitizer: DomSanitizer,
+    private _homeservice: HomeService,
+    private common: CommonService
+  ) {}
 
   ngOnInit(): void {
     this.common.paginaInTrackMetaData();
-    this._homeservice.getHomeInTrack()
-    .subscribe((res:any) => {
+    this._homeservice.getHomeInTrack().subscribe((res: any) => {
       this.loader = false;
       this.data = this._sanitizer.bypassSecurityTrustHtml(res);
       this.data = this.data.changingThisBreaksApplicationSecurity;
       AOS.init();
     });
+  }
+
+  colocarColor(id: number) {
+    $(`#${id}`).toggleClass('colorCliente');
   }
 
   customOptions: OwlOptions = {
@@ -63,30 +90,32 @@ export class IntrackComponent implements OnInit {
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       400: {
-        items: 2
+        items: 2,
       },
       740: {
-        items: 3
+        items: 3,
       },
       940: {
-        items: 8
-      }
+        items: 8,
+      },
     },
-    nav: false
-  }
+    nav: false,
+  };
 
-  abrirSide(){
-    $("#wrapper").toggleClass("toggled");
+  abrirSide() {
+    $('#wrapper').toggleClass('toggled');
     $('.overlaytrabaja').addClass('active');
   }
-  scrollConClick( url:string ){
+  scrollConClick(url: string) {
     console.log(url);
-    $('html, body').animate({
-      scrollTop: $(url).offset().top
-    }, .5);
-}
-
+    $('html, body').animate(
+      {
+        scrollTop: $(url).offset().top,
+      },
+      0.5
+    );
+  }
 }
